597bbdf7be23091eff4b7fa391a7c70223af5949
fredrikgaarden2000 <fredrgaa@stud.ntnu.no>
2025-05-30 12:35:27 +0200

    grgr

--- code/indicator_new.py
+++ code/indicator_new.py
@@ -20,12 +20,12 @@ except FileNotFoundError:
         raise FileNotFoundError("Neither Linux nor Windows path exists")
 
 # Use BASE_DIR in your script
-output_dir = os.path.join(BASE_DIR, "results/large_scale_cont/10_greedy_with_alternatives/greedy_opt_ind/")
+output_dir = os.path.join(BASE_DIR, "Results/Optimal_30/")
 os.makedirs(output_dir, exist_ok=True)
 
 feedstock_df = pd.read_csv(f"{BASE_DIR}aggregated_bavaria_supply_nodes.csv")
-plant_df = pd.read_csv(f"{BASE_DIR}equally_spaced_locations_20.csv")
-distance_df = pd.read_csv(f"{BASE_DIR}Distance_Matrix_20.csv")
+plant_df = pd.read_csv(f"{BASE_DIR}equally_spaced_locations_30.csv")
+distance_df = pd.read_csv(f"{BASE_DIR}Distance_Matrix_30.csv")
 yields_df = pd.read_csv(f"{BASE_DIR}Feedstock_yields.csv")
 
 feedstock_df = feedstock_df[
